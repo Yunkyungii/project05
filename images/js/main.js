0aefa5e5147f82ca0b4a3d71b33c0d4372873961
yunkyung <133613667+Yunkyungii@users.noreply.github.com>
2023-06-09 03:40:18 +0900

    Add files via upload

--- images/js/main.js
+++ images/js/main.js
@@ -14,23 +14,6 @@ $(function () {
         $('.chart_slide').slick('slickNext');
     });
 
-    $('.to_top').on('click', function () {
-        $('html, body').animate({ scrollTop: 0 }, 600)
-    });
-
-    $(window).on('scroll', function () {
-        let sct = $(window).scrollTop();
-        sct > 600 ? $('.to_top').addClass('on') : $('.to_top').removeClass('on');
-        //show, hide   fadeIn, fadeOut 으로 대체 가능
-    });
-
-    $(window).on('scroll', function () {
-        let sct = $(window).scrollTop();
-        sct > 600 ? $('.to_go').addClass('on') : $('.to_go').removeClass('on');
-        //show, hide   fadeIn, fadeOut 으로 대체 가능
-    });
-
-
 });
 
 $(function () {
